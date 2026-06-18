95bfbbe7a9cba2b1b260faa3b8b5fe99541e56f5
Rafa M. <rafaelymelo446@gmail.com>
2026-06-18 14:46:03 -0300

    Modify database schema for responsavel and crianca tables
    Updated the 'crianca' table to include 'data_nascimento' and 'login_id' fields. Fixed foreign key reference and removed the 'id_login' table.

--- DATABASE.sql
+++ DATABASE.sql
@@ -15,18 +15,17 @@
     FOREIGN KEY (crianca_id) REFERENCES crianca(id) ON DELETE CASCADE
 );
 
-    CREATE TABLE id_login (
-    id INT PRIMARY KEY,
-    crianca_id INT NOT NULL,
-    FOREIGN KEY (crianca_id) REFERENCES usuarios(id)
-);
-
     CREATE TABLE crianca (
-    id INT PRIMARY KEY,
-    responsavel_id INT,    
+    id SERIAL PRIMARY KEY,
+    responsavel_id INT NOT NULL,
     nome VARCHAR(100) NOT NULL,
-    parentesco VARCHAR(50),
-    FOREIGN KEY (responsavel_id) REFERENCES responsavel(idON DELETE CASCADE ); 
+    data_nascimento DATE NOT NULL,
+    login_id VARCHAR(20) UNIQUE NOT NULL,
+    senha VARCHAR(255),
+    FOREIGN KEY (responsavel_id)
+    REFERENCES responsavel(id)
+    ON DELETE CASCADE
+);
 	
 	
 SELECT * FROM crianca WHERE EXTRACT(MONTH FROM data_nascimento) = EXTRACT(MONTH FROM CURRENT_DATE);
